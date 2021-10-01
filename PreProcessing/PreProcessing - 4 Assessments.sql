9c5d25e115d626ce2d8d054acad6a8a0f710f267
CarlMHStats <65655378+CarlMHStats@users.noreply.github.com>
2021-10-01 14:24:02 +0100

    Updated prepub table names

--- PreProcessing/PreProcessing - 4 Assessments.sql
+++ PreProcessing/PreProcessing - 4 Assessments.sql
@@ -1,15 +1,9 @@
 
 DECLARE @EndRP INT
-DECLARE @ReportingPeriodStart DATE
-DECLARE @ReportingPeriodEnd DATE
 
 SET @EndRP = (SELECT UniqMonthID
 FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Header]
 WHERE Der_MostRecentFlag = 'Y')
-
-SET @ReportingPeriodStart = (SELECT ReportingPeriodStartDate FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Header] WHERE UniqMonthID = @EndRP) 
-
-SET @ReportingPeriodEnd = (SELECT ReportingPeriodEndDate FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Header] WHERE UniqMonthID = @EndRP+1)
 
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 STEP FOUR - ASSESSMENTS
@@ -44,7 +38,7 @@ SELECT
 WAITFOR DELAY '00:00:01'
 
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-ASSESSMENTS - DELETE PRIMARY DATA FROM LAST MONTH
+ASSESSMENTS - DELETE DATA THAT HAS BEEN SUPERCEDED BY OTHER DATA
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/ 
 
  --LOG START
@@ -53,12 +47,12 @@ INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_QueryStatus]
 
 SELECT
 	@EndRP AS [Month],
-	'Assessments Delete Primary Data Start' AS Step,
+	'Assessments Delete Data Start' AS Step,
 	GETDATE() AS [TimeStamp]
 
 -- START CODE - DELETE DATA
 
-DELETE FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Assessments] WHERE UniqMonthID = @EndRP
+DELETE FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Assessments] WHERE CONCAT(OrgIDProv,UniqMonthID) IN (SELECT CONCAT(OrgIDProvider,UniqMonthID) FROM NHSE_MH_PrePublication.Test.MHS000Header)
 
 -- LOG END
 
@@ -66,7 +60,7 @@ INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_QueryStatus]
 
 SELECT
 	@EndRP AS [Month],
-	'Assessments Delete Primary Data End' AS Step,
+	'Assessments Delete Data End' AS Step,
 	GETDATE() AS [TimeStamp]
 
 WAITFOR DELAY '00:00:01'
@@ -88,6 +82,11 @@ SELECT
 
 SELECT
 	'CON' AS Der_AssTable,
+	h.[ReportingPeriodStartDate],
+	h.[ReportingPeriodEndDate],
+	h.[Der_FY],
+	a.[UniqSubmissionID],
+	a.[NHSEUniqSubmissionID],
 	a.[UniqMonthID],
 	a.[CodedAssToolType],
 	a.[PersScore],
@@ -95,7 +94,7 @@ SELECT
 	a.[RecordNumber],
 	a.[MHS607UniqID] AS Der_AssUniqID,
 	a.[OrgIDProv],
-	a.[Person_ID],
+	a.[Der_Person_ID] AS Person_ID,
 	a.[UniqServReqID],
 	a.[AgeAssessToolCont] AS Der_AgeAssessTool,
 	a.[UniqCareContID],
@@ -103,37 +102,48 @@ SELECT
 
 INTO #Ass
 
-FROM [NHSE_MH_PrePublication].[dbo].[V4_MHS607CodedScoreAssessmentAct] a 
+FROM [NHSE_MH_PrePublication].[Test].[MHS607CodedScoreAssessmentAct] a 
 
-LEFT JOIN [NHSE_MH_PrePublication].[dbo].[V4_MHS201CareContact] c ON a.RecordNumber = c.RecordNumber AND a.UniqServReqID = c.UniqServReqID AND a.UniqCareContID = c.UniqCareContID
+LEFT JOIN [NHSE_MH_PrePublication].[Test].[MHS201CareContact] c ON a.RecordNumber = c.RecordNumber AND a.UniqServReqID = c.UniqServReqID AND a.UniqCareContID = c.UniqCareContID
 
-WHERE a.UniqMonthID >= @EndRP AND a.Der_Use_Submission_Flag = 'Y'  
+LEFT JOIN NHSE_Sandbox_MentalHealth.dbo.PreProc_Header h ON h.UniqMonthID = a.UniqMonthID
 
 INSERT INTO #Ass
 
 SELECT
 	'REF' AS Der_AssTable,
+	h.[ReportingPeriodStartDate],
+	h.[ReportingPeriodEndDate],
+	h.[Der_FY],
+	r.[UniqSubmissionID],
+	r.[NHSEUniqSubmissionID],
 	r.[UniqMonthID],
 	r.[CodedAssToolType],
 	r.[PersScore],
 	r.AssToolCompDate  AS Der_AssToolCompDate,
+	--r.AssToolCompTimestamp AS Der_AssToolCompDate, -- new field for v5
 	r.[RecordNumber],
 	r.[MHS606UniqID] AS Der_AssUniqID,
 	r.[OrgIDProv],
-	r.[Person_ID],
+	r.[Der_Person_ID] AS Person_ID,
 	r.[UniqServReqID],
 	r.AgeAssessToolReferCompDate AS Der_AgeAssessTool,
 	NULL AS [UniqCareContID],
 	NULL AS [UniqCareActID]
 
-FROM [NHSE_MH_PrePublication].[dbo].[V4_MHS606CodedScoreAssessmentRefe] r 
+FROM [NHSE_MH_PrePublication].[Test].[MHS606CodedScoreAssessmentRefer] r 
 
-WHERE r.UniqMonthID >= @EndRP AND r.Der_Use_Submission_Flag = 'Y'  
+LEFT JOIN NHSE_Sandbox_MentalHealth.dbo.PreProc_Header h ON h.UniqMonthID = r.UniqMonthID
 
 INSERT INTO #Ass
 
 SELECT
 	'CLU' AS Der_AssTable,
+	h.[ReportingPeriodStartDate],
+	h.[ReportingPeriodEndDate],
+	h.[Der_FY],
+	a.[UniqSubmissionID],
+	a.[NHSEUniqSubmissionID],
 	c.[UniqMonthID],
 	a.[CodedAssToolType],
 	a.[PersScore],
@@ -141,19 +151,19 @@ SELECT
 	c.[RecordNumber],
 	a.[MHS802UniqID] AS Der_AssUniqID,
 	a.[OrgIDProv],
-	a.[Person_ID],
+	a.[Der_Person_ID] AS Person_ID,
 	r.[UniqServReqID],
 	NULL AS Der_AgeAssessTool,
 	NULL AS [UniqCareContID],
 	NULL AS [UniqCareActID]
 
-FROM [NHSE_MH_PrePublication].[dbo].[V4_MHS802ClusterAssess] a
+FROM [NHSE_MH_PrePublication].[Test].[MHS802ClusterAssess] a
 
-LEFT JOIN [NHSE_MH_PrePublication].[dbo].[V4_MHS801ClusterTool] c ON c.UniqClustID = a.UniqClustID AND c.RecordNumber = a.RecordNumber
+LEFT JOIN [NHSE_MH_PrePublication].[Test].[MHS801ClusterTool] c ON c.UniqClustID = a.UniqClustID AND c.RecordNumber = a.RecordNumber
 
-LEFT JOIN [NHSE_MH_PrePublication].[dbo].[V4_MHS101Referral] r ON r.RecordNumber = c.RecordNumber AND c.AssToolCompDate BETWEEN r.ReferralRequestReceivedDate AND ISNULL(r.ServDischDate,@ReportingPeriodEnd)
+LEFT JOIN NHSE_Sandbox_MentalHealth.dbo.PreProc_Header h ON h.UniqMonthID = a.UniqMonthID
 
-WHERE a.UniqMonthID >= @EndRP AND a.Der_Use_Submission_Flag = 'Y' AND c.AssToolCompDate BETWEEN @ReportingPeriodStart AND @ReportingPeriodEnd
+INNER JOIN [NHSE_MH_PrePublication].[Test].[MHS101Referral] r ON r.RecordNumber = c.RecordNumber AND c.AssToolCompDate BETWEEN r.ReferralRequestReceivedDate AND ISNULL(r.ServDischDate,h.ReportingPeriodEndDate)
 
 -- LOG END
 
@@ -181,7 +191,12 @@ SELECT
 
 --START CODE
 
-SELECT 	
+SELECT 
+	a.[ReportingPeriodStartDate],
+	a.[ReportingPeriodEndDate],
+	a.[Der_FY],
+	a.[UniqSubmissionID],
+	a.[NHSEUniqSubmissionID],
 	a.[Der_AssUniqID],
 	a.[Der_AssTable], 
 	a.[Person_ID],	
@@ -202,14 +217,16 @@ SELECT
 	r.[Lower Range] AS Der_LowerRange,
 	r.[Upper Range] AS Der_UpperRange,
 	CASE 
-		WHEN ISNUMERIC(a.PersScore) = 1 AND a.PersScore <> '-' AND a.PersScore BETWEEN r.[Lower Range] AND r.[Upper Range] THEN 'Y' 
+		WHEN TRY_CONVERT(FLOAT,a.PersScore) BETWEEN r.[Lower Range] AND r.[Upper Range] THEN 'Y' 
 		ELSE NULL 
 	END AS Der_ValidScore,
 	CASE 
 		WHEN ROW_NUMBER () OVER (PARTITION BY a.Person_ID, a.Der_AssToolCompDate, ISNULL(a.UniqServReqID,0), r.[Preferred Term (SNOMED-CT)], a.PersScore ORDER BY a.Der_AssUniqID ASC) = 1
 		THEN 'Y' 
 		ELSE NULL 
-	END AS Der_UniqAssessment
+	END AS Der_UniqAssessment,
+	CONCAT(a.Der_AssToolCompDate,a.UniqServReqID,a.CodedAssToolType,a.PersScore) AS Der_AssKey,
+	CASE WHEN a.Der_AssToolCompDate BETWEEN a.ReportingPeriodStartDate AND a.ReportingPeriodEndDate THEN 1 ELSE 0 END AS Der_AssInMonth
 
 INTO #Ass2
 
@@ -241,43 +258,81 @@ SELECT
 	'Assessments Insert Start' AS Step,
 	GETDATE() AS [TimeStamp]
 
---START CODE
+--START CODE - ASSESSMENT IN MONTH
 
 INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Assessments]
 
 SELECT 
-	a.[Der_AssUniqID],	
-	a.[Der_AssTable], 
-	a.[Person_ID],	
-	a.[UniqMonthID],	
-	a.[OrgIDProv],
-	a.[RecordNumber],	
-	a.[UniqServReqID],	
-	a.[UniqCareContID],	
-	a.[UniqCareActID],		
-	a.[Der_AssToolCompDate],
-	a.[CodedAssToolType],
-	a.[PersScore],
-	a.[Der_AgeAssessTool],
-	a.[Der_AssessmentToolName],
-	a.[Der_PreferredTermSNOMED], 
-	a.[Der_SNOMEDCodeVersion],
-	a.[Der_LowerRange],
-	a.[Der_UpperRange],
-	a.[Der_ValidScore],
-	a.[Der_UniqAssessment],
-	a.[Der_AssessmentCategory],
-	NULL AS Der_AssOrderAsc, 
+	a.ReportingPeriodStartDate,
+	a.ReportingPeriodEndDate,
+	a.Der_FY,
+	a.UniqSubmissionID,
+	a.NHSEUniqSubmissionID,
+	a.UniqMonthID,
+	a.OrgIDProv,
+	a.Person_ID,
+	a.RecordNumber,
+	a.UniqServReqID,
+	a.UniqCareContID,
+	a.UniqCareActID,
+	a.CodedAssToolType,
+	a.PersScore,
+	a.Der_AssUniqID,
+	a.Der_AssTable,
+	a.Der_AssToolCompDate,
+	a.Der_AgeAssessTool,
+	a.Der_AssessmentToolName,
+	a.Der_PreferredTermSNOMED,
+	a.Der_SNOMEDCodeVersion,
+	a.Der_LowerRange,
+	a.Der_UpperRange,
+	a.Der_ValidScore,
+	a.Der_AssessmentCategory,
+	NULL AS Der_AssOrderAsc,
 	NULL AS Der_AssOrderDesc,
-	h.ReportingPeriodStartDate,
-	h.ReportingPeriodEndDate,
-	h.Der_FY
+	a.Der_AssKey
 	
 FROM #Ass2 a
 
-LEFT JOIN NHSE_Sandbox_MentalHealth.dbo.PreProc_Header h ON h.UniqMonthID = a.UniqMonthID
+WHERE Der_UniqAssessment = 'Y' AND Der_AssInMonth = 1
 
-WHERE Der_UniqAssessment = 'Y'
+--START CODE - ASSESSMENT NOT IN MONTH
+
+INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Assessments]
+
+SELECT 
+	a.ReportingPeriodStartDate,
+	a.ReportingPeriodEndDate,
+	a.Der_FY,
+	a.UniqSubmissionID,
+	a.NHSEUniqSubmissionID,
+	a.UniqMonthID,
+	a.OrgIDProv,
+	a.Person_ID,
+	a.RecordNumber,
+	a.UniqServReqID,
+	a.UniqCareContID,
+	a.UniqCareActID,
+	a.CodedAssToolType,
+	a.PersScore,
+	a.Der_AssUniqID,
+	a.Der_AssTable,
+	a.Der_AssToolCompDate,
+	a.Der_AgeAssessTool,
+	a.Der_AssessmentToolName,
+	a.Der_PreferredTermSNOMED,
+	a.Der_SNOMEDCodeVersion,
+	a.Der_LowerRange,
+	a.Der_UpperRange,
+	a.Der_ValidScore,
+	a.Der_AssessmentCategory,
+	NULL AS Der_AssOrderAsc,
+	NULL AS Der_AssOrderDesc,
+	a.Der_AssKey
+	
+FROM #Ass2 a
+
+WHERE Der_UniqAssessment = 'Y' AND Der_AssInMonth = 0 AND a.Der_AssKey NOT IN (SELECT Der_AssKey FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Assessments])
 
 -- LOG END
 
@@ -364,7 +419,7 @@ SELECT
 WAITFOR DELAY '00:00:01'
 
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-ACTIVITY - RECREATE INDEXES
+ASSESSMENT - RECREATE INDEXES
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/ 
 
  --LOG START
