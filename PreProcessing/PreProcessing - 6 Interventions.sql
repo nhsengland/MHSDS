4ecdab2a7096ccc3c4438d8ce02729194d7a3e55
CarlMHStats <65655378+CarlMHStats@users.noreply.github.com>
2021-10-01 14:25:06 +0100

    Updated prepub table names

--- PreProcessing/PreProcessing - 6 Interventions.sql
+++ PreProcessing/PreProcessing - 6 Interventions.sql
@@ -1,12 +1,9 @@
 
 DECLARE @EndRP INT
-DECLARE @ReportingPeriodEnd DATE
 
 SET @EndRP = (SELECT UniqMonthID
 FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Header]
 WHERE Der_MostRecentFlag = 'Y')
-
-SET @ReportingPeriodEnd = (SELECT ReportingPeriodEndDate FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Header] WHERE UniqMonthID = @EndRP)
 
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 STEP SIX - INTERVENTIONS
@@ -41,7 +38,7 @@ SELECT
 WAITFOR DELAY '00:00:01'
 
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-INTERVENTIONS - DELETE PRIMARY DATA FROM LAST MONTH
+INTERVENTIONS - DELETE DATA THAT HAS BEEN SUPERCEDED BY OTHER DATA
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/ 
 
  --LOG START
@@ -50,12 +47,12 @@ INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_QueryStatus]
 
 SELECT
 	@EndRP AS [Month],
-	'Interventions Delete Primary Data Start' AS Step,
+	'Interventions Delete Data Start' AS Step,
 	GETDATE() AS [TimeStamp]
 
 -- START CODE - DELETE DATA
 
-DELETE FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Interventions] WHERE UniqMonthID = @EndRP
+DELETE FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Interventions] WHERE CONCAT(OrgIDProv,UniqMonthID) IN (SELECT CONCAT(OrgIDProvider,UniqMonthID) FROM NHSE_MH_PrePublication.Test.MHS000Header)
 
 -- LOG END
 
@@ -63,7 +60,7 @@ INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_QueryStatus]
 
 SELECT
 	@EndRP AS [Month],
-	'Interventions Delete Primary Data End' AS Step,
+	'Interventions Delete Data End' AS Step,
 	GETDATE() AS [TimeStamp]
 
 WAITFOR DELAY '00:00:01'
@@ -82,24 +79,29 @@ SELECT
 	GETDATE() AS [TimeStamp]
 
 SELECT
+	h.[ReportingPeriodStartDate],
+	h.[ReportingPeriodEndDate],
+	h.[Der_FY],
+	ca.[UniqSubmissionID],
+	ca.[NHSEUniqSubmissionID],
+	ca.[UniqMonthID],
+	ca.[OrgIDProv],
+	ca.[Der_Person_ID] AS Person_ID,
+	ca.[RecordNumber],
+	ca.[UniqServReqID],
+	ca.[UniqCareContID],
+	ca.[UniqCareActID],
+	ca.[FindSchemeInUse],
+	ca.[CodeFind],
+	NULL AS [ObsSchemeInUse],
+	ca.[CodeObs],
+	ca.[ObsValue],
+	ca.[UnitMeasure],
+	ca.[CodeProcAndProcStatus],
 	CAST('DIRECT' As varchar(10)) AS [Der_InterventionType],
 	ca.MHS202UniqID AS [Der_InterventionUniqID],
-	ca.UniqMonthID,
-	ca.OrgIDProv,
-	ca.Person_ID,
-	ca.RecordNumber,
-	ca.UniqServReqID,
-	cc.CareContDate AS Der_ContactDate,
-	ca.UniqCareContID,
-	ca.UniqCareActID,
-	ca.ClinContactDurOfCareAct AS Der_InterventionDuration,
-	ca.FindSchemeInUse,
-	ca.CodeFind,
-	ca.ObsSchemeInUse,
-	ca.CodeObs,
-	ca.ObsValue,
-	ca.UnitMeasure,
-	ca.CodeProcAndProcStatus,
+	cc.CareContDate AS [Der_ContactDate],
+	ca.ClinContactDurOfCareAct AS [Der_InterventionDuration],
 	CASE
 		WHEN CHARINDEX(':',ca.CodeProcAndProcStatus) > 0
 		THEN LEFT(ca.CodeProcAndProcStatus,CHARINDEX(':',ca.CodeProcAndProcStatus)-1)
@@ -109,61 +111,64 @@ SELECT
 		WHEN CHARINDEX('=',ca.CodeProcAndProcStatus) > 0
 		THEN RIGHT(ca.CodeProcAndProcStatus,CHARINDEX('=',REVERSE(ca.CodeProcAndProcStatus))-1)
 		ELSE NULL
-	END AS Der_SNoMEDProcQual,
-	h.ReportingPeriodStartDate,
-	h.ReportingPeriodEndDate,
-	h.Der_FY
-
+	END AS Der_SNoMEDProcQual
+	
 INTO #Int
 
-FROM NHSE_MH_PrePublication.[dbo].[V4_MHS202CareActivity] ca
+FROM NHSE_MH_PrePublication.[Test].[MHS202CareActivity] ca
 
-LEFT JOIN NHSE_MH_PrePublication.[dbo].[V4_MHS201CareContact] cc ON ca.RecordNumber = cc.RecordNumber AND ca.UniqCareContID = cc.UniqCareContID
+LEFT JOIN NHSE_MH_PrePublication.[Test].[MHS201CareContact] cc ON ca.RecordNumber = cc.RecordNumber AND ca.UniqCareContID = cc.UniqCareContID
 
 LEFT JOIN NHSE_Sandbox_MentalHealth.dbo.PreProc_Header h ON h.UniqMonthID = ca.UniqMonthID 
 
-WHERE ca.UniqMonthID >= @EndRP AND ca.Der_Use_Submission_Flag = 'Y' AND (ca.CodeFind IS NOT NULL OR ca.CodeObs IS NOT NULL OR ca.CodeProcAndProcStatus IS NOT NULL)
+WHERE (ca.CodeFind IS NOT NULL OR ca.CodeObs IS NOT NULL OR ca.CodeProcAndProcStatus IS NOT NULL)
 
 INSERT INTO #Int
 
 SELECT 
+	h.[ReportingPeriodStartDate],
+	h.[ReportingPeriodEndDate],
+	h.[Der_FY],
+	i.[UniqSubmissionID],
+	i.[NHSEUniqSubmissionID],
+	i.[UniqMonthID],
+	i.[OrgIDProv],
+	i.[Der_Person_ID] AS Person_ID,
+	i.[RecordNumber],
+	i.[UniqServReqID],
+	NULL AS [UniqCareContID],
+	NULL AS [UniqCareActID],
+	i.[FindSchemeInUse],
+	i.[CodeFind],
+	NULL AS [ObsSchemeInUse],
+	NULL AS [CodeObs],
+	NULL AS [ObsValue],
+	NULL AS [UnitMeasure],
+	i.CodeProcAndProcStatus,
+	--i.[CodeIndActProcAndProcStatus] AS [CodeProcAndProcStatus], --new for v5
 	'INDIRECT' AS [Der_InterventionType],
 	i.MHS204UniqID AS [Der_InterventionUniqID],
-	i.UniqMonthID,
-	i.OrgIDProv,
-	i.Person_ID,
-	i.RecordNumber,
-	i.UniqServReqID,
-	i.IndirectActDate AS Der_ContactDate,
-	NULL AS UniqCareContID,
-	NULL AS UniqCareActID,
-	i.DurationIndirectAct AS Der_InterventionDuration,
-	i.FindSchemeInUse,
-	i.CodeFind,
-	NULL AS ObsSchemeInUse,
-	NULL AS CodeObs,
-	NULL AS ObsValue,
-	NULL AS UnitMeasure,
-	i.CodeProcAndProcStatus,
+	i.IndirectActDate AS [Der_ContactDate],
+	i.DurationIndirectAct AS [Der_InterventionDuration],
 	CASE
 		WHEN CHARINDEX(':',i.CodeProcAndProcStatus) > 0
 		THEN LEFT(i.CodeProcAndProcStatus,CHARINDEX(':',i.CodeProcAndProcStatus)-1)
 		ELSE i.CodeProcAndProcStatus
+		--WHEN CHARINDEX(':',i.CodeIndActProcAndProcStatus) > 0 --new for v5
+		--THEN LEFT(i.CodeIndActProcAndProcStatus,CHARINDEX(':',i.CodeIndActProcAndProcStatus)-1) --new for v5
+		--ELSE i.CodeIndActProcAndProcStatus --new for v5
 	END AS Der_SNoMEDProcCode,
 	CASE
 		WHEN CHARINDEX('=',i.CodeProcAndProcStatus) > 0
 		THEN RIGHT(i.CodeProcAndProcStatus,CHARINDEX('=',REVERSE(i.CodeProcAndProcStatus))-1)
 		ELSE NULL
-	END AS Der_SNoMEDProcQual,
-	h.ReportingPeriodStartDate,
-	h.ReportingPeriodEndDate,
-	h.Der_FY
+	END AS Der_SNoMEDProcQual
 
-FROM NHSE_MH_PrePublication.dbo.V4_MHS204IndirectActivity i
+FROM NHSE_MH_PrePublication.Test.MHS204IndirectActivity i
 
 LEFT JOIN NHSE_Sandbox_MentalHealth.dbo.PreProc_Header h ON h.UniqMonthID = i.UniqMonthID 
 
-WHERE i.UniqMonthID >= @EndRP AND i.Der_Use_Submission_Flag = 'Y' AND (i.CodeFind IS NOT NULL OR i.CodeProcAndProcStatus IS NOT NULL)
+WHERE (i.CodeFind IS NOT NULL OR i.CodeProcAndProcStatus IS NOT NULL)
 
 -- LOG END
 
@@ -193,36 +198,38 @@ SELECT
 
 INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Interventions]
 
-SELECT
-	i.Der_InterventionType,
-	i.Der_InterventionUniqID,
-	i.UniqMonthID,
-	i.OrgIDProv,
-	i.Person_ID,
-	i.RecordNumber,
-	i.UniqServReqID,
-	i.Der_ContactDate,
-	i.UniqCareContID,
-	i.UniqCareActID,
-	i.Der_InterventionDuration,
-	i.FindSchemeInUse,
-	i.CodeFind,
+	SELECT
+	i.[ReportingPeriodStartDate],
+	i.[ReportingPeriodEndDate],
+	i.[Der_FY],
+	i.[UniqSubmissionID],
+	i.[NHSEUniqSubmissionID],
+	i.[UniqMonthID],
+	i.[OrgIDProv],
+	i.[Person_ID],
+	i.[RecordNumber],
+	i.[UniqServReqID],
+	i.[UniqCareContID],
+	i.[UniqCareActID],
+	i.[FindSchemeInUse],
+	i.[CodeFind],
+	i.[ObsSchemeInUse],
+	i.[CodeObs],
+	i.[ObsValue],
+	i.[UnitMeasure],
+	i.[CodeProcAndProcStatus],
+	i.[Der_InterventionType],
+	i.[Der_InterventionUniqID],
+	i.[Der_ContactDate],
+	i.[Der_InterventionDuration],
 	s2.Term AS Der_SNoMEDFindTerm,
 	CASE WHEN s2.Term IS NULL THEN NULL WHEN s2.Effective_To IS NULL OR s2.Effective_To > i.Der_ContactDate THEN 'Current' ELSE 'Retired' END AS Der_SNoMEDFindValidity,
-	i.ObsSchemeInUse,
-	i.CodeObs,
 	s3.Term AS Der_SNoMEDObsTerm,
 	CASE WHEN s3.Term IS NULL THEN NULL WHEN s3.Effective_To IS NULL OR s3.Effective_To > i.Der_ContactDate THEN 'Current' ELSE 'Retired' END AS Der_SNoMEDObsValidity,
-	i.ObsValue,
-	i.UnitMeasure,
-	i.CodeProcAndProcStatus,
-	i.Der_SNoMEDProcCode,
-	i.Der_SNoMEDProcQual,
+	i.[Der_SNoMEDProcCode],
+	i.[Der_SNoMEDProcQual],
 	s1.Term AS Der_SNoMEDProcTerm,
-	CASE WHEN s1.Term IS NULL THEN NULL WHEN s1.Effective_To IS NULL OR s1.Effective_To > i.Der_ContactDate THEN 'Current' ELSE 'Retired' END AS Der_SNoMEDProcValidity,
-	i.ReportingPeriodStartDate,
-	i.ReportingPeriodEndDate,
-	i.Der_FY
+	CASE WHEN s1.Term IS NULL THEN NULL WHEN s1.Effective_To IS NULL OR s1.Effective_To > i.Der_ContactDate THEN 'Current' ELSE 'Retired' END AS Der_SNoMEDProcValidity
 
 FROM #Int i
 
