af5d243595d0b18e3cad41f18d4c54f2d5afb030
CarlMHStats <65655378+CarlMHStats@users.noreply.github.com>
2022-07-21 15:59:40 +0100

    Update PreProcessing - 7 Record Counts.sql
    Updated to include monthly logging of pre proc record counts

--- PreProcessing/PreProcessing - 7 Record Counts.sql
+++ PreProcessing/PreProcessing - 7 Record Counts.sql
@@ -1,4 +1,3 @@
-
 DECLARE @EndRP INT
 DECLARE @ReportingPeriodEnd DATE
 
@@ -7,34 +6,6 @@ FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Header]
 WHERE Der_MostRecentFlag = 'Y')
 
 SET @ReportingPeriodEnd = (SELECT ReportingPeriodEndDate FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Header] WHERE UniqMonthID = @EndRP)
-
-/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-PREPROC RECORD COUNTS - DELETE PRIMARY DATA FROM LAST MONTH
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/ 
-
- --LOG START
-
-INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_QueryStatus]
-
-SELECT
-	@EndRP AS [Month],
-	'Preproc Delete Primary Data Start' AS Step,
-	GETDATE() AS [TimeStamp]
-
--- START CODE - DELETE DATA
-
-DELETE FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_RecordCounts] WHERE UniqMonthID = @EndRP AND SubmissionType = 'PreProc'
-
--- LOG END
-
-INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_QueryStatus]
-
-SELECT
-	@EndRP AS [Month],
-	'Preproc Delete Primary Data End' AS Step,
-	GETDATE() AS [TimeStamp]
-
-WAITFOR DELAY '00:00:01'
 
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 RECORD COUNTS - INSERT DATA
@@ -52,39 +23,33 @@ SELECT
 -- START CODE
 
 INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_RecordCounts] 
-	SELECT 'PreProc_Activity' AS [TableName], NULL AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
+	SELECT 'PreProc_Activity' AS [TableName], @EndRP AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
 	FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Activity] 
-	WHERE UniqMonthID >= @EndRP 
 	GROUP BY UniqMonthID 
 
 INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_RecordCounts] 
-	SELECT 'PreProc_Assessments' AS [TableName], NULL AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
+	SELECT 'PreProc_Assessments' AS [TableName], @EndRP AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
 	FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Assessments] 
-	WHERE UniqMonthID >= @EndRP 
 	GROUP BY UniqMonthID 
 
 INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_RecordCounts] 
-	SELECT 'PreProc_Inpatients' AS [TableName], NULL AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
+	SELECT 'PreProc_Inpatients' AS [TableName], @EndRP AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
 	FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Inpatients] 
-	WHERE UniqMonthID >= @EndRP 
 	GROUP BY UniqMonthID 
 
 INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_RecordCounts] 
-	SELECT 'PreProc_Interventions' AS [TableName], NULL AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
+	SELECT 'PreProc_Interventions' AS [TableName], @EndRP AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
 	FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Interventions] 
-	WHERE UniqMonthID >= @EndRP 
 	GROUP BY UniqMonthID 
 
 INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_RecordCounts] 
-	SELECT 'PreProc_Header' AS [TableName], NULL AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
+	SELECT 'PreProc_Header' AS [TableName], @EndRP AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
 	FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Header] 
-	WHERE UniqMonthID >= @EndRP 
 	GROUP BY UniqMonthID 
 
 INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_RecordCounts] 
-	SELECT 'PreProc_Referral' AS [TableName], NULL AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
+	SELECT 'PreProc_Referral' AS [TableName], @EndRP AS OrgIDProvider, UniqMonthID, 'PreProc' AS SubmissionType, COUNT (*) AS [RecordCount] 
 	FROM [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_Referral] 
-	WHERE UniqMonthID >= @EndRP 
 	GROUP BY UniqMonthID 
 
 --LOG END
@@ -93,7 +58,7 @@ INSERT INTO [NHSE_Sandbox_MentalHealth].[dbo].[PreProc_QueryStatus]
 
 SELECT
 	@EndRP AS [Month],
-	'Record Counts Insert End' AS Step,
+	'PreProc Record Counts Insert End' AS Step,
 	GETDATE() AS [TimeStamp]
 
 WAITFOR DELAY '00:00:01'
@@ -123,3 +88,5 @@ SELECT
 	@EndRP AS [Month],
 	'Record Counts Create Index End' AS Step,
 	GETDATE() AS [TimeStamp]
+
+END
